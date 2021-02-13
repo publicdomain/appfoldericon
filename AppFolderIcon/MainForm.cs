6499e9a21a20d5897b26bc84e761d8857315d15e
Victor L. Senior <publicdomainvic@gmail.com>
2021-02-13 06:25:49 -0400

    Add OnOptionsToolStripMenuItemDropDownItemClicked() code

--- AppFolderIcon/MainForm.cs
+++ AppFolderIcon/MainForm.cs
@@ -72,7 +72,14 @@ namespace AppFolderIcon
         /// <param name="e">Event arguments.</param>
         private void OnOptionsToolStripMenuItemDropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
-            // TODO Add code
+            // Set tool strip menu item
+            ToolStripMenuItem toolStripMenuItem = (ToolStripMenuItem)e.ClickedItem;
+
+            // Toggle checked
+            toolStripMenuItem.Checked = !toolStripMenuItem.Checked;
+
+            // Set topmost by check box
+            this.TopMost = this.alwaysOnTopToolStripMenuItem.Checked;
         }
 
         /// <summary>
